b1c34da9e55bd90366751d903eba6330abdb5c5e
mariaperezdeayalas <mperezdeayalas@gmail.com>
2021-11-15 13:50:59 +0100

    cleaning terminadoooo

--- SRC/functions.py
+++ SRC/functions.py
@@ -1,4 +1,8 @@
-import requests 
+import requests
+from dotenv import load_dotenv
+import os 
+from functools import reduce
+import operator
 
 def geocode(direccion):
     """
@@ -9,3 +13,50 @@ def geocode(direccion):
         return {"type": "Point", "coordinates": [data["latt"], data["longt"]]}
     except:
         return data
+
+def get_parameters(city, *queries):
+    """
+    I have built this function to get the parameters of each query I want to see
+    for each city
+    """
+    url_query = 'https://api.foursquare.com/v2/venues/explore'
+    client_id = os.getenv("tok1")
+    client_secret = os.getenv("tok2")
+    d = {}
+    for i in queries:
+        parameters = {
+            'client_id': client_id,
+            'client_secret': client_secret,
+            'v': '20180323',
+            'll': f"{city[0]}, {city[1]}",
+            'query': i,
+            'radius': '5000'
+        }
+       
+        resp = requests.get(url_query, params = parameters).json()
+        d[i] = resp
+    
+    return d
+
+def getFromDict(diccionario,mapa):
+    return reduce(operator.getitem,diccionario)
+
+def type_point(lista):
+    return {"type":"Point", "coordinates": lista}
+
+def clean_data(json, lista):
+    info = {"nombre": "name", "latitud": ["location", "lat"], "longitud": ["location", "lng"], "categoría": ["categories", "shortName"]} 
+    total = []
+    
+    for elemento in json:
+        place = {}
+        # for key, value in info.items():
+        place["name"] = json.get("name")
+        place["lat"] = json["location"]["lat"]
+        place["lng"] = json["location"]["lng"]
+        place["shortName"] = json["categories"][0]["shortName"]
+            #place = {key: getFromDict(json, value) for key,value in info.items()}
+    
+        place["location"] = type_point([place['lat'], place['lng']])
+        total.append(place)
+    return place
